1a13ee9ecb1e7d81abff18e51370404364b2c6b2
Agata99 <agasutera@gmail.com>
2024-01-24 11:27:29 +0100

    Aggiunta file

---
+++ app.py
@@ -0,0 +1,11 @@
+from credentials import BOT_TOKEN, BOT_USERNAME
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
