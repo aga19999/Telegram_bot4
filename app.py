429a80f8a5f0979eafd8abdb2c772072321a12de
Agata99 <agasutera@gmail.com>
2024-01-31 11:24:41 +0100

    prova e app v2

--- app.py
+++ app.py
@@ -2,16 +2,7 @@ from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, WebAppInfo
 from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler, MessageHandler, filters, ContextTypes
 from credentials import BOT_TOKEN, BOT_USERNAME
 import logging
-"""import mysql.connector"""
 import json
-
-"""mydb = mysql.connector.connect(
-    host = 'sql11.freemysqlhosting.net',
-    user = 'sql11676026',
-    password = 'iiCRnW6sxu',
-    database = 'sql11676026',
-)
-print(mydb)"""
 CHAT_IDS = []
 
 logging.basicConfig(
@@ -31,15 +22,7 @@ async def web_app_data(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     """Stampa i dati ricevuti e rimuove il bottone"""
     data = json.loads(update.effective_message.web_app_data.data)
     print(data)
-    """Qui memorizzo i dati nel DB"""
-    """mycursor = mydb.cursor()
-    sql = "INSERT INTO utenti (id, nome) VALUES (%s, %s)"
-    val = (0, data["nome"])
-    mycursor.execute(sql, val)
 
-    mydb.commit()
-
-    print(mycursor.rowcount, "record inserted.")"""
 
 async def help(update: Update, callback: CallbackContext):
     await update.message.reply_text("Questo bot è rivolto al personale scolastico che intende interagire con" +
@@ -47,7 +30,7 @@ async def help(update: Update, callback: CallbackContext):
 
 async def web_app_data(update: Update, context: CallbackContext):
     data = json.loads(update.message.web_app_data.data)
-    await update.message.reply_text("Your data was:")
+    await update.message.reply_text("Ecco i dati raccolti:")
     for result in data:
         await update.message.reply_text(f"{result['name']}: {result['value']}")
 
