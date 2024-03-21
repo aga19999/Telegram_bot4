04e5fa188b7ad54c127ec6582e243385c57df483
Agata99 <agasutera@gmail.com>
2024-03-21 14:15:19 +0100

    prova v 0.2

--- prova.py
+++ prova.py
@@ -1,5 +1,5 @@
 import logging
-from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, WebAppInfo, Bot
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove, WebAppInfo, Bot
 from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler, MessageHandler, filters, ContextTypes, Updater
 from credentials import BOT_TOKEN, BOT_USERNAME, PASSWORD_DATABASE, MIO_ID_UTENTE
 import pymysql.cursors
@@ -30,6 +30,9 @@ async def help(update: Update, callback: CallbackContext):
     await update.message.reply_text("Questo bot è rivolto al personale scolastico che intende interagire con" +
                                     "gli studenti tramite l'uso di sondaggi e altri widget")
 
+    # Questo bot è rivolto ai pazienti e permette di metterli in comunicazione con il loro terapeuta mediante
+    # l'invio di questionari
+
 
 async def web_app_data(update: Update, context: CallbackContext):
     data = json.loads(update.message.web_app_data.data)
@@ -45,25 +48,25 @@ async def web_app_data(update: Update, context: CallbackContext):
 
         # Inserisce l'utente
         with connection.cursor() as cursor:
-            query_check_user = "SELECT * FROM utenti WHERE codice_utente = %s"
-            cursor.execute(query_check_user, (str(user_id),))
+            query_check_user = "SELECT * FROM utenti WHERE telegram_user_id = %s"
+            cursor.execute(query_check_user, (str(user_id)))
             result = cursor.fetchone()
 
             if not result:
-                query_insert_user = "INSERT INTO utenti (codice_utente) VALUES (%s)"
-                cursor.execute(query_insert_user, (str(user_id),))
+                query_insert_user = "INSERT INTO utenti (telegram_user_id) VALUES (%s)"
+                cursor.execute(query_insert_user, (str(user_id)))
                 connection.commit()
 
         # Ottiene l'id dell'utente dalla tabella utenti
         with connection.cursor() as cursor:
             # Prende l'id dalla tabella utenti e lo mette nella tabella rilevazioni
-            codice_utente = "SELECT codice_utente FROM utenti WHERE codice_utente = %s"
+            codice_utente = "SELECT telegram_user_id FROM utenti WHERE telegram_user_id = %s"
             cursor.execute(codice_utente, (str(user_id)))
             user_result = cursor.fetchone()
 
             if user_result:
-                user_id_from_table = user_result["codice_utente"]
-                query = "INSERT INTO rilevazioni (id_utente, risposta, orarioInvio, orarioRisposta) VALUES (%s, %s, NULL, NULL)"
+                user_id_from_table = user_result["telegram_user_id"]
+                query = "INSERT INTO rilevazioni (id_utente, risposta) VALUES (%s, %s)"
                 cursor.execute(query, (user_id_from_table, risposta_json))
                 connection.commit()
 
@@ -79,7 +82,7 @@ async def mostra_dati_raccolti(update: Update, context: CallbackContext):
             query = "SELECT risposta FROM rilevazioni WHERE id_utente = %s ORDER BY id DESC LIMIT 1"
             # La SELECT in basso serve per ottenere invece tutte le risposte dell'utente
             # SELECT risposta FROM rilevazioni WHERE id_utente = %s
-            cursor.execute(query, (user_id))
+            cursor.execute(query, user_id)
             risposte = cursor.fetchall()
 
             # Elabora o stampa le risposte a seconda delle tue esigenze
@@ -89,15 +92,18 @@ async def mostra_dati_raccolti(update: Update, context: CallbackContext):
     except Exception as e:
         print(f"Errore durante il recupero delle risposte: {e}")
 
-
 async def invia_questionario(context: ContextTypes.DEFAULT_TYPE) -> None:
     data = context.job.data
-    url = f"Clicca qui per completare il questionario: {data["survey"]}"
+    url = data["survey"]
     print(url)
     # Manda il questionario all'utente
-    print(f"Invio il questionario a {data["telegram_user_id"]} - Link: {url}")
-    await context.bot.send_message(chat_id=data["telegram_user_id"], text=url)
+    kb = [
+        [KeyboardButton("Clicca qui!", web_app=WebAppInfo(url))]
+    ]
+    rKM = ReplyKeyboardMarkup(kb, one_time_keyboard=True) # Il secondo parametro nasconde la tastiera dopo l'utilizzo
+    await context.bot.send_message(chat_id=data["telegram_user_id"], text="Nuovo questionario", reply_markup=rKM)
 
+    print(f"Invio il questionario a {data["telegram_user_id"]} - Link: {url}")
 async def fetch_elenco_sessioni(context: ContextTypes.DEFAULT_TYPE) -> None:
     with connection.cursor() as cursor:
         # Seleziona le sessioni nel prossimo minuto
@@ -108,8 +114,8 @@ async def fetch_elenco_sessioni(context: ContextTypes.DEFAULT_TYPE) -> None:
                     INNER JOIN utenti u ON u.id = us.id_utente 
                     WHERE dataInvio > %s AND dataInvio <= %s AND stato = 0"""
         now = datetime.now()
-        half_before = now - timedelta(minutes=60)
-        cursor.execute(query, (half_before, now))
+        one_hours_before = now - timedelta(minutes=60)
+        cursor.execute(query, (one_hours_before, now))
         elenco_sessioni = cursor.fetchall()
         print(elenco_sessioni)
         for sessione in elenco_sessioni:
@@ -121,27 +127,12 @@ async def fetch_elenco_sessioni(context: ContextTypes.DEFAULT_TYPE) -> None:
                 connection.commit()
                 print(f"Stato della sessione aggiornato a 1 per l'utente {sessione["id_utente"]}")
 
-async def aggiungi_sessioni_prova(context: ContextTypes.DEFAULT_TYPE) -> None:
-    try:
-        with connection.cursor() as cursor:
-            query = "INSERT INTO sessioni (dataInvio, id_utente, survey) VALUES (%s, %s, %s)"
-            now = datetime.now()
-            id_utente = MIO_ID_UTENTE
-            messaggio = "Ehii"
-            cursor.execute(query, (now, id_utente, messaggio))
-            connection.commit()
-            print("Query aggiunta con successo al database.")
-    except Exception as e:
-        print(f"Errore durante l'aggiunta della query al database: {e}")
-
 if __name__ == '__main__':
 
     print(f"Il bot è in uso! Usalo cliccando qui: http://t.me/{BOT_USERNAME} !")
     application = ApplicationBuilder().token(BOT_TOKEN).build()
     job_queue = application.job_queue
     job_queue.run_repeating(fetch_elenco_sessioni, interval=10.0, first=0.0)
-    #job_queue.run_repeating(aggiungi_sessioni_prova, interval=60.0, first=0.0)
-    #job_queue.run_repeating(controlla_invii, interval=10.0, first=0.0)
     application.add_handler(CommandHandler('start', launch_web_ui))
     application.add_handler(CommandHandler('help', help))
     application.add_handler(CommandHandler('show_answers', mostra_dati_raccolti))
