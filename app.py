dd37a70e52653ecd67deaffad0b37db37fb5424a
Agata99 <agasutera@gmail.com>
2024-04-22 09:44:49 +0200

    versione finale

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove, WebAppInfo, Bot
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove, WebAppInfo, Bot, \
+    LinkPreviewOptions, helpers, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.constants import ParseMode
 from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler, MessageHandler, filters, ContextTypes
 from credentials import BOT_TOKEN, BOT_USERNAME, PASSWORD_DATABASE
 import pymysql.cursors
@@ -26,12 +28,8 @@ async def launch_web_ui(update: Update, callback: CallbackContext):
 
 
 async def help(update: Update, callback: CallbackContext):
-    await update.message.reply_text("""Questo bot è rivolto ai pazienti e permette di metterli in comunicazione"
-                                    con il loro terapeuta attraverso la compilazione di questionari""")
-
-    # Questo bot è rivolto al personale scolastico che intende interagire con
-    # gli studenti tramite l'uso di sondaggi e altri widget
-
+    await update.message.reply_text("""Questo bot è rivolto ai terapeuti e permette di metterli in comunicazione"
+                                    con il loro pazienti attraverso l'invio di questionari""")
 
 async def web_app_data(update: Update, context: CallbackContext):
     data = json.loads(update.message.web_app_data.data)
@@ -83,8 +81,7 @@ async def web_app_data(update: Update, context: CallbackContext):
                     connection.commit()
                     # Manda una risposta all'utente dopo aver salvato i dati ricevuti
                     await context.bot.send_message(chat_id=user_result["telegram_user_id"],
-                                                   text="Grazie per aver compilato il sondaggio!",
-                                                   reply_markup=ReplyKeyboardRemove())
+                                                   text="Grazie per aver compilato il sondaggio!")
 
     except Exception as e:
         print(f"Errore durante l'elaborazione dei dati: {e}")
@@ -124,44 +121,38 @@ async def invia_questionario(context: ContextTypes.DEFAULT_TYPE) -> None:
                                    reply_markup=ReplyKeyboardMarkup(kb))
     print(f"Invio il questionario a {data["telegram_user_id"]} - Link: {url}")
 
-
 async def reminder_utente(context: ContextTypes.DEFAULT_TYPE) -> None:
     try:
         data = context.job.data
         url = data["survey"]
         id_utente = data["telegram_user_id"]
         id_sessione = data["id_sessione"]
-        now = datetime.now() - timedelta(hours=2)
 
         with connection.cursor() as cursor:
-            # Per evitare delle eccezioni seleziona il numero di righe a cui è associata l'id della sessione
-            # Se il valore è 0 allora l'utente non ha compilato il questionario
-            # Se il valore è 1 allora l'utente ha già compilato il questionario e non verrà mandato il reminder
             query = """SELECT *
                         FROM rilevazioni 
                         WHERE id_sessione = %s AND id_utente = %s"""
             cursor.execute(query, (id_sessione, id_utente))
             rilevazione = cursor.fetchone()
 
-
-            # Se il conteggio è zero, invia il reminder
             if rilevazione is None:
                 # Richiamo la funzione di timeout nel caso in cui anche dopo il reminder l'utente decida di non
                 # compilare il questionario
-                # context.job_queue.run_once(timeout_sondaggio, now + timedelta(seconds=20), data=data)
+                # context.job_queue.run_once(timeout_sondaggio, now + timedelta(seconds=60), data=data)
                 kb = [
-                    [KeyboardButton("Clicca qui!", web_app=WebAppInfo(url))]
+                    [InlineKeyboardButton("Clicca qui!", web_app=WebAppInfo(url))]
                 ]
+                reply_markup = InlineKeyboardMarkup(kb)
                 await context.bot.send_message(chat_id=id_utente,
-                                               text="Ehi, ti sei dimenticato di compilare il questionario! Clicca qui sotto!",
-                                               reply_markup=ReplyKeyboardMarkup(kb))
+                                               text="Ehi, ti sei dimenticato di compilare il questionario!",
+                                               reply_markup=reply_markup)
+
     except Exception as e:
         logging.error(
             f"Si è verificato un errore: {type(e).__name__} - {e}")
 
 
-""" Se l'utente dopo il reminder non compila il questionario,
-                    esaurisce la possibilità di farlo """
+# Se l'utente dopo il reminder non compila il questionario, esaurisce la possibilità di farlo
 async def timeout_sondaggio(context: ContextTypes.DEFAULT_TYPE) -> None:
     try:
         data = context.job.data
@@ -169,16 +160,13 @@ async def timeout_sondaggio(context: ContextTypes.DEFAULT_TYPE) -> None:
         id_sessione = data["id_sessione"]
 
         with connection.cursor() as cursor:
-            # Per evitare delle eccezioni seleziona il numero di righe a cui è associata l'id della sessione
-            # Se il valore è 0 allora l'utente non ha compilato il questionario
-            # Se il valore è 1 allora l'utente ha già compilato il questionario e non verrà mandato il reminder
-            query = """SELECT COUNT(*)
-                                   FROM rilevazioni 
-                                   WHERE id_sessione = %s"""
-            cursor.execute(query, (id_sessione,))
-            count = cursor.fetchone()  # Ottieni il valore del conteggio
-            # Se il conteggio è zero, invia il reminder
-            if count['COUNT(*)'] == 0:
+            query = """SELECT *
+                        FROM rilevazioni 
+                        WHERE id_sessione = %s AND id_utente = %s"""
+            cursor.execute(query, (id_sessione, id_utente))
+            rilevazione = cursor.fetchone()
+
+            if rilevazione is None:
                 await context.bot.send_message(chat_id=id_utente,
                                                text="Ops! Tempo scaduto. Sondaggio non più disponibile",
                                                reply_markup=ReplyKeyboardRemove())
@@ -204,8 +192,9 @@ async def fetch_elenco_sessioni(context: ContextTypes.DEFAULT_TYPE) -> None:
                 print(elenco)
 
         for sessione in elenco_sessioni:
-            context.job_queue.run_once(invia_questionario, (sessione["dataInvio"] - timedelta(hours=2)), data=sessione)
-            # Aggiorna lo status della sessione evitando che venga ripetuta più volte
+            context.job_queue.run_once(invia_questionario, (sessione["dataInvio"] - timedelta(hours=2)),
+                                       data=sessione)
+            # Aggiorna lo status della sessione per evitare che venga messa in coda più volte
             with connection.cursor() as cursor1:
                 query_update_status = "UPDATE utenti_sessioni SET stato = 1 WHERE id_utente = %s AND id_sessione = %s"
                 cursor1.execute(query_update_status, (sessione["id_utente"], sessione["id_sessione"]))
@@ -228,7 +217,7 @@ if __name__ == '__main__':
         Comandi per telegram:
         start - Scopri cosa può fare questo bot!
         help - Chiedi assistenza al bot
-        show_answers - Visualizza i risultati
+        show_answers - Visualizza i risultati dell'ultimo questionario
         https://aga19999.github.io/Telegram_bot4/survey1.html
         https://aga19999.github.io/Telegram_bot4/survey2.html
     """
